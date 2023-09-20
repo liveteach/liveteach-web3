28370c9a9acc95d0248af62e1d852aad11845d8a
frankie <frankie@vegascity.org>
2023-09-20 13:21:28 +0100

    TEACH-51 Add Classroom admin list functions

--- smart-contract/test/TeachContractClassroomAdmin_test.js
+++ smart-contract/test/TeachContractClassroomAdmin_test.js
@@ -2,7 +2,8 @@ const { assert, expect } = require("chai");
 
 describe("TeachContractClassroomAdmin", function () {
   let owner;
-  let otherUser;
+  let user1;
+  let user2;
   let teachContract;
   let randomWallet = "0xAA14f5F645273Aa6411995Bf8F02557B7C74a154";
 
@@ -10,26 +11,53 @@ describe("TeachContractClassroomAdmin", function () {
     teachContract = await ethers.deployContract("contracts/TeachContract.sol:TeachContract");
     let accounts = await ethers.getSigners();
     owner = accounts[0];
-    otherUser = accounts[1];
+    user1 = accounts[1];
+    user2 = accounts[2];
   })
 
   // classroom admin
   it("Can create classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    let result = await teachContract.connect(owner).isClassroomAdmin(otherUser);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    let result = await teachContract.connect(owner).isClassroomAdmin(user1);
     assert.equal("true", result.toString()); // string conversion to assert actual true rather than truthy value
   });
 
+  it("Can get all classroom admins", async function () {
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user2, [5, 6]);
+
+    let result = await teachContract.connect(owner).getClassroomAdmins();
+    assert.equal(2, result.length);
+    let classroomAdmin1 = result[0];
+    let classroomAdmin2 = result[1];
+
+    assert.equal(user1.address, classroomAdmin1.walletAddress);
+    assert.equal(user2.address, classroomAdmin2.walletAddress);
+
+    assert.equal([1n, 2n, 3n, 4n].toString(), classroomAdmin1.landIds);
+    assert.equal([5n, 6n].toString(), classroomAdmin2.landIds);
+  });
+
+  it("Can get single classroom admin", async function () {
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user2, [5, 6]);
+
+    let classroomAdmin1 = await teachContract.connect(owner).getClassroomAdmin(user1.address);
+
+    assert.equal(user1.address, classroomAdmin1.walletAddress);
+    assert.equal([1n, 2n, 3n, 4n].toString(), classroomAdmin1.landIds);
+  });
+
   it("Can add land to existing classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    let result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    let result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(4, result.length);
     assert.equal(1, result[0]);
     assert.equal(2, result[1]);
     assert.equal(3, result[2]);
     assert.equal(4, result[3]);
-    await teachContract.connect(owner).addClassroomAdminLandIds(otherUser, [5, 6]);
-    result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).addClassroomAdminLandIds(user1, [5, 6]);
+    result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(6, result.length);
     assert.equal(1, result[0]);
     assert.equal(2, result[1]);
@@ -42,12 +70,12 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can remove specific land from existing classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    let result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    let result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(4, result.length);
     assert.isTrue(await teachContract.connect(owner).isClassroomAdminAssignedLandIds([1, 2, 3, 4]));
-    await teachContract.connect(owner).removeClassroomAdminLandIds(otherUser, [2, 4]);
-    result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).removeClassroomAdminLandIds(user1, [2, 4]);
+    result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(2, result.length);
     assert.equal(1, result[0]);
     assert.equal(3, result[1]);
@@ -57,12 +85,12 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can remove all land from existing classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    let result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    let result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(4, result.length);
     assert.isTrue(await teachContract.connect(owner).isClassroomAdminAssignedLandIds([1, 2, 3, 4]));
-    await teachContract.connect(owner).removeAllClassroomAdminLandIds(otherUser);
-    result = await teachContract.connect(owner).getClassroomAdminLandIds(otherUser);
+    await teachContract.connect(owner).removeAllClassroomAdminLandIds(user1);
+    result = await teachContract.connect(owner).getClassroomAdminLandIds(user1);
     assert.equal(0, result.length);
     assert.isFalse(await teachContract.connect(owner).isClassroomAdminAssignedLandId(1));
     assert.isFalse(await teachContract.connect(owner).isClassroomAdminAssignedLandId(2));
@@ -71,12 +99,17 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can remove existing classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     assert.isTrue(await teachContract.connect(owner).isClassroomAdminAssignedLandIds([1, 2, 3, 4]));
-    let result = await teachContract.connect(owner).isClassroomAdmin(otherUser);
+    let result = await teachContract.connect(owner).isClassroomAdmin(user1);
     assert.equal("true", result.toString());
-    await teachContract.connect(owner).removeClassroomAdmin(otherUser);
-    result = await teachContract.connect(owner).isClassroomAdmin(otherUser);
+    await teachContract.connect(owner).removeClassroomAdmin(user1);
+    result = await teachContract.connect(owner).getClassroomAdmins();
+    assert.equal(0, result.length);
+    result = await expect(teachContract.connect(owner).getClassroomAdmin(user1.address))
+      .to.be.revertedWith("Classroom admin not found.");
+    console.log(result);
+    result = await teachContract.connect(owner).isClassroomAdmin(user1);
     assert.equal("false", result.toString());
     assert.isFalse(await teachContract.connect(owner).isClassroomAdminAssignedLandId(1));
     assert.isFalse(await teachContract.connect(owner).isClassroomAdminAssignedLandId(2));
@@ -85,13 +118,13 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can't create classroom admin if already classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    await expect(teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]))
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    await expect(teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]))
       .to.be.revertedWith("Provided wallet address is already CLASSROOM_ADMIN");
   });
 
   it("Can't add land to non classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await expect(teachContract.connect(owner).addClassroomAdminLandIds(randomWallet, [5, 6]))
       .to.be.revertedWith("Provided wallet address is not CLASSROOM_ADMIN");
 
@@ -100,7 +133,7 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can't remove specific land from non classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await expect(teachContract.connect(owner).removeClassroomAdminLandIds(randomWallet, [2, 4]))
       .to.be.revertedWith("Provided wallet address is not CLASSROOM_ADMIN");
 
@@ -109,7 +142,7 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can't remove all land from non classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await expect(teachContract.connect(owner).removeAllClassroomAdminLandIds(randomWallet))
       .to.be.revertedWith("Provided wallet address is not CLASSROOM_ADMIN");
 
@@ -117,20 +150,20 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can't remove non classroom admin", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await expect(teachContract.connect(owner).removeClassroomAdmin(randomWallet))
       .to.be.revertedWith("Provided wallet address is not CLASSROOM_ADMIN");
     assert.isTrue(await teachContract.connect(owner).isClassroomAdminAssignedLandIds([1, 2, 3, 4]));
   });
 
   it("Can't add same landId to classroom admin twice", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
-    await expect(teachContract.connect(owner).addClassroomAdminLandIds(otherUser, [4]))
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
+    await expect(teachContract.connect(owner).addClassroomAdminLandIds(user1, [4]))
       .to.be.revertedWith("Land ID already assigned");
   });
 
   it("Can't add already assigned landId to different classroom admin on create", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await expect(teachContract.connect(owner).createClassroomAdmin(randomWallet, [4, 5, 6]))
       .to.be.revertedWith("Land ID already assigned");
     let result = await teachContract.connect(owner).isClassroomAdmin(randomWallet);
@@ -141,7 +174,7 @@ describe("TeachContractClassroomAdmin", function () {
   });
 
   it("Can't add already assigned landId to different classroom admin on add", async function () {
-    await teachContract.connect(owner).createClassroomAdmin(otherUser, [1, 2, 3, 4]);
+    await teachContract.connect(owner).createClassroomAdmin(user1, [1, 2, 3, 4]);
     await teachContract.connect(owner).createClassroomAdmin(randomWallet, [5, 6]);
     await expect(teachContract.connect(owner).addClassroomAdminLandIds(randomWallet, [1, 2]))
       .to.be.revertedWith("Land ID already assigned");
