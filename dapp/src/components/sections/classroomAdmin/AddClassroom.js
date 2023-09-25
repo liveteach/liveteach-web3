d20b3afdbc2fad3ec40a5736cf1979fb832e6b8a
shaunMcelhinney <shaun@vegascity.org>
2023-09-25 12:59:30 +0100

    TEACH-92 missing header full and full width input

--- dapp/src/components/sections/classroomAdmin/AddClassroom.js
+++ dapp/src/components/sections/classroomAdmin/AddClassroom.js
@@ -103,8 +103,9 @@ export function AddClassroom(props){
             <div className="ui container">
                 <Grid container>
                     <Grid item xs={6}>
-                        <div style={{backgroundColor: 'white', color: 'black',width: '90%'}}>
-                            <MuiChipsInput value={classLandIds} onChange={handleChange} />
+                        <h4>LAND Parcels</h4>
+                        <div style={{backgroundColor: 'white', color: 'black'}}>
+                            <MuiChipsInput value={classLandIds} onChange={handleChange} fullWidth={true}/>
                         </div>
                     </Grid>
                     <Grid item xs={6}>
