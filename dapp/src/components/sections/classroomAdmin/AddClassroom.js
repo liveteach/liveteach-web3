c174191feffd0d0a6f30b51a7a279968c404d496
shaunMcelhinney <shaun@vegascity.org>
2023-09-25 10:32:09 +0100

    TEACH-92 Parcel selector upgraded with working map

--- dapp/src/components/sections/classroomAdmin/AddClassroom.js
+++ dapp/src/components/sections/classroomAdmin/AddClassroom.js
@@ -1,16 +1,47 @@
-import {TextField} from "@mui/material";
+import {Grid, TextField} from "@mui/material";
+import {useEffect} from "react";
+import {setImgEndpoint, setLandParcels} from "../../../store/classroomAdminState";
 import {useDispatch, useSelector} from "react-redux";
 import {setClassName, setClassLandIds} from "../../../store/classroomAdminState";
+import { MuiChipsInput} from "mui-chips-input";
+import {createClassroom} from '../../../utils/interact';
 
 export function AddClassroom(props){
 
-    const {className, classLandIds} = useSelector((state) => state.classroomAdmin)
+    const {className, classLandIds, imgEndpoint} = useSelector((state) => state.classroomAdmin)
     const dispatch = useDispatch()
+    const baseUrl = "https://api.decentraland.org/v2/map.png?"
 
-    function handleSplit(ids){
-        let arr = ids.split(",");
-        arr = arr.map(item => item.trim().replace(/\n/g, ''));
-        return arr;
+    const handleChange = (newChips) => {
+        dispatch(setClassLandIds(newChips))
+    }
+
+    useEffect(() => {
+        const parsedParcels = classLandIds.join(';')
+        const logicalCentre = logicalCentreCoord(classLandIds);
+        dispatch(setImgEndpoint(baseUrl + "center=" + logicalCentre + "&size=30&selected=" + parsedParcels))
+    },[classLandIds])
+
+    function logicalCentreCoord(coords){
+        let sumLat = 0;
+        let sumLon = 0;
+
+        for (const coord of coords) {
+            const [lat, lon] = coord.split(',').map(parseFloat);
+
+            if (!isNaN(lat) && !isNaN(lon)) {
+                sumLat += lat;
+                sumLon += lon;
+            }
+        }
+
+        const averageLatitude = sumLat / coords.length;
+        const averageLongitude = sumLon / coords.length;
+
+        const logicalCenter = [Math.round(averageLatitude), Math.round(averageLongitude)];
+
+        console.log("Logical Center Coordinate:", logicalCenter);
+        return logicalCenter;
     }
 
     return(
@@ -19,7 +50,17 @@ export function AddClassroom(props){
                 <div className="ui container">
                     <div className="dcl tabs">
                         <div className="dcl tabs-left">
-                            <h4>Classroom Administrators</h4>
+                            <h4>Add Classroom</h4>
+                        </div>
+                        <div className="dcl tabs-right">
+                                <button
+                                    onClick={() => {
+                                        createClassroom(className,classLandIds).then(result => {
+                                            console.log(result)
+                                        })
+                                    }}
+                                    className="ui small primary button"
+                                >Add</button>
                         </div>
                     </div>
                 </div>
@@ -44,6 +85,18 @@ export function AddClassroom(props){
                         disabled
                     />
                 </div>
+            </div>
+            <div className="ui container">
+                <Grid container>
+                    <Grid item xs={6}>
+                        <div style={{backgroundColor: 'white', color: 'black',width: '90%'}}>
+                            <MuiChipsInput value={classLandIds} onChange={handleChange} />
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                            <img src={imgEndpoint} style={{width: '40%'}}/>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )
