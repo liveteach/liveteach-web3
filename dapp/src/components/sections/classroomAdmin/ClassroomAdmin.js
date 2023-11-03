65652e449f86e5f6dcd178a80031af5860ecd92d
Shaun Mcelhinney <shaun@vegascity.org>
2023-11-03 11:24:08 +0000

    TEACH-139 classroom admin page hooked up to pending transactions

--- dapp/src/components/sections/classroomAdmin/ClassroomAdmin.js
+++ dapp/src/components/sections/classroomAdmin/ClassroomAdmin.js
@@ -1,14 +1,14 @@
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {Button} from "@mui/material";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import { getClassrooms, deleteClassroom, getTeachers, deleteTeacher } from "../../../utils/interact";
 import {setClassrooms, setTeachers} from "../../../store/classroomAdminState";
 import {NoAdmittance} from "../NoAdmittance";
 
 export default function ClassroomAdmin(props){
 
-    const { classrooms, teachers } = useSelector((state) => state.classroomAdmin)
+    const { classrooms, teachers, pendingTeachers, pendingClassrooms } = useSelector((state) => state.classroomAdmin)
     const {roles} = useSelector((state) => state.adminUser)
     const render = roles.includes("classroomAdmin")
 
@@ -54,6 +54,7 @@ export default function ClassroomAdmin(props){
                             <tr>
                                 <th>Classroom Name</th>
                                 <th>Classroom Id</th>
+                                <th>Classroom Coords</th>
                                 <th>Classroom GUID</th>
                                 <th></th>
                             </tr>
@@ -64,9 +65,29 @@ export default function ClassroomAdmin(props){
                                         <tr key={`Contributor_${index}`}>
                                             <td>
                                                 {item.name}
+                                                <span style={{color:'green', display: 'none'}} id={`classroomRemove${index}`}>Pending..</span>
                                             </td>
                                             <td>
                                                 {item.id}
+                                            </td>
+                                            <td>
+                                                {item["landCoordinates"] && Array.isArray(item["landCoordinates"]) ? (
+                                                    <div className="coordinates-container">
+                                                        {item["landCoordinates"]
+                                                            .map((coords, index) => (
+                                                                <span key={index} className="coordinate-item">
+                                                                    {index < 2 ? `(${coords[0]},${coords[1]})` : ''}
+                                                                    {index === 1 && item["landCoordinates"].length > 2 && (
+                                                                                                                    <span className="tooltip">
+                                                                            {item["landCoordinates"].map((coords, i) => `(${coords[0]},${coords[1]})`).join("  ")}
+                                                                        </span>
+                                                                    )}
+                                                                </span>
+                                                            ))}..
+                                                    </div>
+                                                ) : (
+                                                    <div>Coordinates not available</div>
+                                                )}
                                             </td>
                                             <td>
                                                 {item.guid}
@@ -74,13 +95,34 @@ export default function ClassroomAdmin(props){
                                             <td>
                                                 <Button
                                                 onClick={() =>{
+                                                    let text = document.getElementById(`classroomRemove${index}`);
+                                                    text.style.display = 'block'
                                                     deleteClassroom(item.id).then(result => {
-                                                        console.log(result)
+                                                        text.style.display = 'none'
+                                                        getClassrooms().then(result => {
+                                                            dispatch(setClassrooms(result))
+                                                        })
                                                     })
                                                 }}>Remove</Button>
                                             </td>
                                         </tr>
                                     );
+                                })
+                            }
+                            {
+                                pendingClassrooms.map((item, index) => {
+                                    if (item.name !== '' && item.status !== '') {
+                                        return (
+                                            <tr key={'classroom_pending_' + index}>
+                                                <td>{item.name}</td>
+                                                <td>
+                                                    <span style={{ color: 'green' }}>{item.status}</span>
+                                                </td>
+                                            </tr>
+                                        );
+                                    } else {
+                                        return null;
+                                    }
                                 })
                             }
                             </tbody>
@@ -123,7 +165,8 @@ export default function ClassroomAdmin(props){
                                     return (
                                         <tr key={`Contributor_${index}`}>
                                             <td>
-                                                {item.walletAddress}
+                                                {item.walletAddress }
+                                                <span style={{color:'green', display: 'none'}} id={`teacherRemove${index}`}>Pending..</span>
                                             </td>
                                             <td>
                                                 {item.classroomIds}
@@ -131,7 +174,14 @@ export default function ClassroomAdmin(props){
                                             <td>
                                                 <Button
                                                     onClick={() => {
+                                                        let text = document.getElementById(`teacherRemove${index}`);
+                                                        text.style.display = 'block'
                                                         deleteTeacher(item.walletAddress).then(result => {
+                                                            text.style.display = 'none'
+                                                            getTeachers().then(result => {
+                                                                console.log(result)
+                                                                dispatch(setTeachers(result))
+                                                            })
                                                             console.log(result)
                                                         })
                                                     }}
@@ -139,6 +189,22 @@ export default function ClassroomAdmin(props){
                                             </td>
                                         </tr>
                                     );
+                                })
+                            }
+                            {
+                                pendingTeachers.map((item, index) => {
+                                    if (item.name !== '' && item.status !== '') {
+                                        return (
+                                            <tr key={'teacher_pending_' + index}>
+                                                <td>{item.name}</td>
+                                                <td>
+                                                    <span style={{ color: 'green' }}>{item.status}</span>
+                                                </td>
+                                            </tr>
+                                        );
+                                    } else {
+                                        return null;
+                                    }
                                 })
                             }
                             </tbody>
