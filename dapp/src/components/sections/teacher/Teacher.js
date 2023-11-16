717dc972671a3dd7d3d7b1dd7386190f1be528a7
Shaun Mcelhinney <shaun@vegascity.org>
2023-11-16 14:18:27 +0000

    TEACH-154 add roles dependancy to useEffect to ensure updating of teachers

--- dapp/src/components/sections/teacher/Teacher.js
+++ dapp/src/components/sections/teacher/Teacher.js
@@ -37,7 +37,7 @@ export default function Teacher(props){
                 dispatch(setClassIds(result.classroomIds))
             })
         }
-    },[])
+    },[roles])
 
     return(
         <div className="ui container">
