c3de3c9bf1a9576e41f52d03c21d12469ded2846
frankie <frankie@vegascity.org>
2023-12-28 14:31:42 +0000

    TEACH-289 DCL Worlds

--- smart-contract/test/LiveTeach/LiveTeachUtils.js
+++ smart-contract/test/LiveTeach/LiveTeachUtils.js
@@ -4,18 +4,26 @@ module.exports = class Utils {
 
   static teachContract;
   static landContract;
+  static dclRegistrarContract;
 
   static accounts;
 
   static owner;
   static operator;
+  static worldOwner;
+
+  static worldName = "decentraland";
+  static worldName2 = "jumboland";
 
   static user1;
   static user2;
 
   static async init() {
+    this.entityContract = await ethers.deployContract("contracts/ContractUtils.sol:ContractUtils");
     this.teachContract = await ethers.deployContract("contracts/LiveTeach.sol:LiveTeach");
     this.landContract = await ethers.deployContract("contracts/references/LANDRegistry.sol:LANDRegistry");
+    this.dclRegistrarContract = await ethers.deployContract("contracts/references/DCLRegistrar.sol:DCLRegistrar");
+
     this.accounts = await ethers.getSigners();
 
     this.owner = this.accounts[0];
@@ -25,9 +33,17 @@ module.exports = class Utils {
     this.user3 = this.accounts[4];
     this.user4 = this.accounts[5];
     this.user5 = this.accounts[6];
+    this.worldOwner = this.accounts[7];
 
     let landContractAddress = await this.landContract.target;
+    let entityManagerContractAddress = await this.entityContract.target;
+    let teachContractAddress = await this.teachContract.target;
+    let dclRegistrarAddress = await this.dclRegistrarContract.target;
+
+    await this.entityContract.connect(this.owner).addAdmin(teachContractAddress);
+    await this.teachContract.connect(this.owner).setContractUtils(entityManagerContractAddress);
     await this.teachContract.connect(this.owner).setLANDRegistry(landContractAddress);
+    await this.teachContract.connect(this.owner).setDCLRegistrar(dclRegistrarAddress);
 
     let parcels = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10], [0, 11], [0, 12], [0, 13], [0, 14], [0, 15], [0, 16], [0, 17], [0, 18], [0, 19], [0, 20], [0, 21], [1, 1], [-55, 1], [-55, 2], [-55, 3], [-55, 4]];
     let xParcels = [];
@@ -40,6 +56,10 @@ module.exports = class Utils {
     let assetIds = await this.teachContract.connect(this.owner).getLandIdsFromCoordinates(parcels);
     await this.landContract.connect(this.owner).assignMultipleParcels(xParcels, yParcels, this.owner);
     await this.landContract.connect(this.owner).setManyUpdateOperator([...assetIds], this.operator);
+    await this.dclRegistrarContract.connect(this.owner).setOwnerOf(this.worldName, this.worldOwner);
+    await this.dclRegistrarContract.connect(this.owner).setOwnerOf(this.worldName2, this.worldOwner);
+
+
   }
 
   static getGuid() {
