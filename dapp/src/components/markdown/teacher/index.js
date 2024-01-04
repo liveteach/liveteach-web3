b84cfff7aac7b46040f9bf6195ff7db54c9bc026
Shaun Mcelhinney <shaun@vegascity.org>
2024-01-04 11:40:27 +0000

    TEACH-214 using the podium docs added

--- dapp/src/components/markdown/teacher/index.js
+++ dapp/src/components/markdown/teacher/index.js
@@ -4,5 +4,6 @@ import models from  "./models.md";
 import polls from  "./polls.md";
 import publish from  "./publish.md";
 import special from "./special.md";
+import activation from "./activatingcontent.md"
 
-export { teacherConfigCreation, imagesAndVideo, polls, publish, special, models }
+export { teacherConfigCreation, imagesAndVideo, polls, publish, special, models, activation }
